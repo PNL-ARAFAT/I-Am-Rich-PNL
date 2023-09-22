828250b8c067c28a783278c0e32107353cc48015
PNL-ARAFAT <akash.arafat@playnext-lab.co.jp>
2023-09-22 15:19:28 +0900

    Initial Commit

---
+++ I Am Rich PNL/I_Am_Rich_PNLApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_PNLApp.swift
+//  I Am Rich PNL
+//
+//  Created by Akash Arafat on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_PNLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
